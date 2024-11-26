eeaf24723214fb70b9b13160ca941bf0175e4acb
mshortcodes <mshortcodes@gmail.com>
2024-11-26 12:36:49 -0800

    add counter

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+type apiConfig struct {
+	count int
+	mu    sync.Mutex
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -14,9 +21,22 @@ func main() {
 		Handler: mux,
 	}
 
+	var apiCfg apiConfig
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
 
+	mux.HandleFunc("GET /count", apiCfg.handlerCountInc)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerCountInc(w http.ResponseWriter, r *http.Request) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	cfg.count++
+
+	countMsg := fmt.Sprintf("The count is %d", cfg.count)
+	w.Write([]byte(countMsg))
 }
