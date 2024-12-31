2079b0941c185daa698c22b7d8b728a34fa3af4d
mshortcodes <mshortcodes@gmail.com>
2024-12-31 10:59:50 -0800

    secure GET /api/logs

--- handler_logs_get.go
+++ handler_logs_get.go
@@ -7,6 +7,12 @@ import (
 	"github.com/google/uuid"
 )
 
+type PracticeLogSafe struct {
+	Date       string `json:"date"`
+	ColorDepth int32  `json:"color_depth"`
+	Confirmed  bool   `json:"confirmed"`
+}
+
 // handlerLogsGet returns logs by user ID if provided, otherwise returns all logs.
 func (cfg *apiConfig) handlerLogsGet(w http.ResponseWriter, r *http.Request) {
 	dbLogs, err := cfg.db.GetLogs(r.Context())
@@ -25,22 +31,31 @@ func (cfg *apiConfig) handlerLogsGet(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	var logs []PracticeLog
+	var logs []any
 
 	for _, dbLog := range dbLogs {
 		if userID != uuid.Nil && dbLog.UserID != userID {
 			continue
 		}
 
-		log := PracticeLog{
-			Id:         dbLog.ID,
-			Date:       dbLog.Date.Format(time.DateOnly),
-			ColorDepth: dbLog.ColorDepth,
-			Confirmed:  dbLog.Confirmed,
-			UserID:     dbLog.UserID,
+		if userID != uuid.Nil {
+			log := PracticeLog{
+				Id:         dbLog.ID,
+				Date:       dbLog.Date.Format(time.DateOnly),
+				ColorDepth: dbLog.ColorDepth,
+				Confirmed:  dbLog.Confirmed,
+				UserID:     dbLog.UserID,
+			}
+			logs = append(logs, log)
+		} else {
+			log := PracticeLogSafe{
+				Date:       dbLog.Date.Format(time.DateOnly),
+				ColorDepth: dbLog.ColorDepth,
+				Confirmed:  dbLog.Confirmed,
+			}
+			logs = append(logs, log)
 		}
 
-		logs = append(logs, log)
 	}
 
 	respondWithJSON(w, http.StatusOK, logs)
